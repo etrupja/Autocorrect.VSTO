067fadfa1d8ba32896b40012faf466c9705efe75
Nertil Poci <nertilpoci@hotmail.com>
2019-01-04 13:34:05 +0100

    Update Constants.cs

--- Autocorrect.Common/Constants.cs
+++ Autocorrect.Common/Constants.cs
@@ -6,7 +6,7 @@ namespace Autocorrect.Common
 {
   public static  class AppConstants
     {
-        public static string ApiBaseUri = "https://localhost:5001";
+        public static string ApiBaseUri = "https://autocorrectapi.azurewebsites.net";
         public static string SyncUri = $"{ApiBaseUri}/api/SpecialWords/getallwords";
         public static string ValidateLicenseUri = $"{ApiBaseUri}/api/license/isValid/";
         public static string UpdateUtilizationUri = $"{ApiBaseUri}/api/license/setusage/";
