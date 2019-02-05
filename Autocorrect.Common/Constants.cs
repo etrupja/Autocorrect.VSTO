80331055d08707d0a36cf4c525eba2ff1093c10d
ERVIS-PC\Ervis <ervistrupja@outlook.com>
2019-02-05 11:30:07 +0100

    ApiBaseUri updated

--- Autocorrect.Common/Constants.cs
+++ Autocorrect.Common/Constants.cs
@@ -6,7 +6,7 @@ namespace Autocorrect.Common
 {
   public static  class AppConstants
     {
-        public static string ApiBaseUri = "https://api.tekstsakte.com";
+        public static string ApiBaseUri = "https://dashboard.tekstsakte.com";
         public static string SyncUri = $"{ApiBaseUri}/api/sync/all/";
         public static string ValidateLicenseUri = $"{ApiBaseUri}/api/license/isValid/";
         public static string UpdateUtilizationUri = $"{ApiBaseUri}/api/license/setusage/";
